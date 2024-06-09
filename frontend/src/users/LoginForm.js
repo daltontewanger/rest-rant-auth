df8f65d2e571ee920889db28d5ea0741d67a98fe
Dalton Tewanger <daltontewanger@yahoo.com>
2024-06-09 10:24:16 -0700

    implemented JWT for persisting logged in user

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -16,20 +16,26 @@ function LoginForm() {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/authentication/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    });
-    const data = await response.json();
+    try {
+      const response = await fetch("http://localhost:5000/authentication/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
+      const data = await response.json();
 
-    if (response.status === 200) {
-      setCurrentUser(data.user);
-      history.push("/");
-    } else {
-      setErrorMessage(data.message);
+      if (response.status === 200) {
+        setCurrentUser(data.user);
+        localStorage.setItem("token", data.token);
+        history.push("/");
+      } else {
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+      setErrorMessage("An error occurred, please try again");
     }
   }
 
